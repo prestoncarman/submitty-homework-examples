9b1dfa800d8762d98236f2bedff060a85b67fcd8
Preston Carman <preston.carman@wallawalla.edu>
2022-04-22 09:36:02 -0700

    Cleaning up submitty config

--- verify_submitty_examples.sh
+++ verify_submitty_examples.sh
@@ -2,6 +2,7 @@
 
 # Pre clean up of python build folders
 find . -type d -name __pycache__ -prune -exec rm -rf {} \;
+find . -type d -name .pytest_cache -prune -exec rm -rf {} \;
 
 # Run the submitty test for each submission
 for i in */ 
