154b50fef41d8372677bf4e6caaae821907df6d6
Preston Carman <preston.carman@wallawalla.edu>
2022-04-22 09:53:04 -0700

    Saving option for multiple successful or failing submissions

--- verify_submitty_examples.sh
+++ verify_submitty_examples.sh
@@ -4,62 +4,67 @@
 find . -type d -name __pycache__ -prune -exec rm -rf {} \;
 find . -type d -name .pytest_cache -prune -exec rm -rf {} \;
 
-# Run the submitty test for each submission
+# Check each example
 for i in */ 
 do 
   [[ -d "$i" ]] || break
-  echo "Running ${i}"; 
+  echo "Running ${i} example"; 
 
   cd "${i}" || exit
 
-  # Copy over test files and submission
-  rm -rf tmp
-
-  mkdir tmp
-  cp config/test_input/* tmp/
-  cp submissions/solution/* tmp/
-
-  # Execute test
-  cd tmp || exit
-  pipenv run python test_submitty.py 1> output.txt 2> error.txt
-  status=$?
-  if [[ $status -eq 0 ]]
-  then
-    echo "--- Successful - Confirmed solution ---"
-  else
-    echo "--- Failed solution ---"
-    cat output.txt 
-    echo "--- Failed solution ---"
-    exit 1
-  fi
-
-  # clean up
-  cd .. || exit
-  rm -rf tmp
+  # Run the submitty test for each submission
+  for i in submissions/*/ 
+  do 
+    [[ -d "$i" ]] || break
+    echo " - Check submission ${i}"; 
 
 
-  # Failed submission
-  mkdir tmp
-  cp config/test_input/* tmp/
-  cp submissions/failing/* tmp/
+    # Copy over test files and submission
+    rm -rf tmp
 
-  # Execute test
-  cd tmp || exit
-  python3 test_submitty.py 1> output.txt 2> error.txt
-  status=$?
-  if [[ $status -ne 0 ]]
-  then
-    echo "--- Successful - Found incorrect submission ---"
-  else
-    echo "--- Failed to find error ---"
-    cat output.txt 
-    echo "--- Failed to find error ---"
-    exit 1
-  fi
+    mkdir tmp
+    cp config/test_input/* tmp/
+    cp "${i}"* tmp/
 
-  # clean up
-  cd .. || exit
-  rm -rf tmp
+    # Execute test
+    cd tmp || exit
+    pipenv run python test_submitty.py 1> output.txt 2> error.txt
+    status=$?
+
+    if [[ $i == *"failing"* ]]
+    then
+
+      # Expected failing submission
+      if [[ $status -ne 0 ]]
+      then
+        echo "   * Successful - Found incorrect submission"
+      else
+        echo "   --- Failed to find error ---"
+        cat output.txt 
+        echo "   --- Failed to find error ---"
+        exit 1
+      fi
+
+    else 
+
+      # Expecte successful submission
+      if [[ $status -eq 0 ]]
+      then
+        echo "   * Successful - Confirmed solution"
+      else
+        echo "   --- Failed solution ---"
+        cat output.txt 
+        echo "   --- Failed solution ---"
+        exit 1
+      fi
+    fi
+
+    # clean up
+    cd .. || exit
+    rm -rf tmp
+
+  # end of submission
+  done
 
   # Move back up to project root.
   cd .. || exit
