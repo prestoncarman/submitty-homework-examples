df2019e1b53b3caa4b367f757cf52f8a2637f54e
Preston Carman <preston.carman@wallawalla.edu>
2022-04-28 21:07:28 -0700

    Adding mock example

---
+++ python_pytest_mock/README.md
@@ -0,0 +1,12 @@
+# Python Pytest Mock Example
+
+The test checks for correctly using `math.sqrt()` method from the `math` library.
+
+## Tests
+
+* Check the parameters passed to `math.sqrt()`
+* Provides a specified `math.sqrt()` return value to be used in test
+
+## Solutions
+
+Provided a solution simple solution.
---
+++ python_pytest_mock/config/test_input/test_submitty.py
@@ -0,0 +1,29 @@
+import sys
+import unittest
+from unittest.mock import patch
+
+import pytest
+import submission
+
+
+class TestMock(unittest.TestCase):
+    @patch("submission.math.sqrt")
+    def test_sqrt_method_calls(self, mock_math):
+        # Alternate return for sqrt mock method
+        mock_math.return_value = 32
+
+        # Test call using 16
+        number = 16
+        result = submission.find_square_root(number)
+
+        # Assert mock was called with 16
+        mock_math.assert_called_with(number)
+
+        # Assert the method returned mock return value
+        assert result == 32, "Function is not using math.sqrt() return value"
+
+
+if __name__ == "__main__":
+    exit_code = pytest.main(["--tb=short", "--no-header", "test_submitty.py"])
+    print(exit_code, file=sys.stderr)
+    sys.exit(int(exit_code.value))
