eeababf68e839c90f848e2db110a297515e679c8
Preston Carman <preston.carman@wallawalla.edu>
2022-06-01 09:24:30 -0700

    Formatting fix

--- python_pytest_custom_report/config/test_input/test_submitty.py
+++ python_pytest_custom_report/config/test_input/test_submitty.py
@@ -10,20 +10,27 @@ def test_hello_world(record_xml_attribute):
         submission.hello_world() == "Hello World!"
     ), "Function return does not match expected value"
 
+
 def test_hello_world_length(record_xml_attribute):
     record_xml_attribute("name", "Function returns a string of 12 characters")
     assert (
         len(submission.hello_world()) == 12
     ), "Function return does not match expected string length"
 
+
 def test_hello_world_first_character(record_xml_attribute):
     record_xml_attribute("name", "Function returns string that starts with 'H'")
-    assert (
-        submission.hello_world()[0] == "H"
-    ), "Function return does not start with 'H'"
+    assert submission.hello_world()[0] == "H", "Function return does not start with 'H'"
 
 
 if __name__ == "__main__":
-    exit_code = pytest.main(["--junitxml=pytest_results.xml", "--tb=short", "--no-header", "test_submitty.py"])
+    exit_code = pytest.main(
+        [
+            "--junitxml=pytest_results.xml",
+            "--tb=short",
+            "--no-header",
+            "test_submitty.py",
+        ]
+    )
     print(exit_code, file=sys.stderr)
     sys.exit(int(exit_code.value))
