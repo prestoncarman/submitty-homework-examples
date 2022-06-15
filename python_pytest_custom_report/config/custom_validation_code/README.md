41cd08c79c07357a3d26e9a85cc75f8691edb439
Preston Carman <preston.carman@wallawalla.edu>
2022-06-14 22:00:09 -0700

    Adding the validator config file and test files (#10)
    * Adding the validator config file and test files
    
    * Trying alternate config.

---
+++ python_pytest_custom_report/config/custom_validation_code/README.md
@@ -0,0 +1,4 @@
+# Pytest Custom Validator
+
+The code needed for the custom validation is in `grader.py`.
+All other files are used to test the `grader.py` file and are not needed for the custom validator.
--- python_pytest_custom_report/config/custom_validation_code/grader_test.py
+++ python_pytest_custom_report/config/custom_validation_code/grader_test.py
@@ -17,13 +17,29 @@ def test_log_line():
 
 
 @patch("sys.exit")
-def test_return_result(mock_sys_exit):
-    expected_json = '{\n    "status": "success",\n    '
-    expected_json += '"data": {\n        "score": 1,\n        "message": "done",\n        "status": "success"\n    }\n}'
-
+def test_return_result_success(mock_sys_exit):
     grader.return_result(1, "done", "success")
-    with open("validation_results.json") as jsonfile:
-        assert jsonfile.read() == expected_json
+    with open("validation_results.json") as actual_file:
+        with open(
+            "python_pytest_custom_report/config/custom_validation_code/sample_result_1.json"
+        ) as expected_file:
+            assert actual_file.read() == expected_file.read()
+
+    # Test clean up
+    if os.path.exists("validation_results.json"):
+        os.remove("validation_results.json")
+
+    mock_sys_exit.assert_called_with(0)
+
+
+@patch("sys.exit")
+def test_return_result_failure(mock_sys_exit):
+    grader.return_result(0, "done", "failure")
+    with open("validation_results.json") as actual_file:
+        with open(
+            "python_pytest_custom_report/config/custom_validation_code/sample_result_3.json"
+        ) as expected_file:
+            assert actual_file.read() == expected_file.read()
 
     # Test clean up
     if os.path.exists("validation_results.json"):
@@ -34,11 +50,12 @@ def test_return_result(mock_sys_exit):
 
 @patch("sys.exit")
 def test_return_error(mock_sys_exit):
-    expected_json = '{\n    "status": "fail",\n    "message": "done"\n}'
-
     grader.return_error("done")
-    with open("validation_results.json") as jsonfile:
-        assert jsonfile.read() == expected_json
+    with open("validation_results.json") as actual_file:
+        with open(
+            "python_pytest_custom_report/config/custom_validation_code/sample_validation_failure.json"
+        ) as expected_file:
+            assert actual_file.read() == expected_file.read()
 
     # Test clean up
     if os.path.exists("validation_results.json"):
@@ -49,37 +66,38 @@ def test_return_error(mock_sys_exit):
 
 def test_parse_pytest_xml_fail():
     expected = [
-        {"name": "Function returns 'Hello World!'", "result": False},
-        {"name": "Function returns a string of 12 characters", "result": False},
-        {"name": "Function returns string that starts with 'H'", "result": False},
+        {"name": "Function returns 'Hello World!'", "result": "failure"},
+        {"name": "Function returns a string of 12 characters", "result": "failure"},
+        {"name": "Function returns string that starts with 'H'", "result": "failure"},
     ]
     actual = grader.parse_pytest_xml(
-        "python_pytest_custom_report/config/custom_validation_code/sample_failure.xml"
+        ["python_pytest_custom_report/config/custom_validation_code/sample_failure.xml"]
     )
+    print(actual)
     assert actual == expected
 
 
 def test_parse_pytest_xml_pass():
     expected = [
-        {"name": "Function returns 'Hello World!'", "result": True},
-        {"name": "Function returns a string of 12 characters", "result": True},
-        {"name": "Function returns string that starts with 'H'", "result": True},
+        {"name": "Function returns 'Hello World!'", "result": "success"},
+        {"name": "Function returns a string of 12 characters", "result": "success"},
+        {"name": "Function returns string that starts with 'H'", "result": "success"},
     ]
     actual = grader.parse_pytest_xml(
-        "python_pytest_custom_report/config/custom_validation_code/sample_success.xml"
+        ["python_pytest_custom_report/config/custom_validation_code/sample_success.xml"]
     )
     assert actual == expected
 
 
 def test_print_result_fail():
-    actual = grader.get_pytest_results([{"name": "Test 1", "result": False}])
+    actual = grader.get_pytest_results([{"name": "Test 1", "result": "failure"}])
     assert (
         actual == "Tests\n----------------------------------------\n1. FAIL   Test 1\n"
     ), "Single failed test does not return expected output"
 
 
 def test_print_result_pass():
-    pytest_object = [{"name": "Test 1", "result": True}]
+    pytest_object = [{"name": "Test 1", "result": "success"}]
     actual = grader.get_pytest_results(pytest_object)
     assert (
         actual == "Tests\n----------------------------------------\n1. PASS   Test 1\n"
@@ -88,13 +106,14 @@ def test_print_result_pass():
 
 @patch("sys.exit")
 def test_grade_pytest_results(mock_sys_exit):
-    pytest_object = [{"name": "Test 1", "result": True}]
-    expected_json = '{\n    "status": "success",\n    "data": {\n        '
-    expected_json += '"score": 1,\n        "message": "Test 1",\n        "status": "success"\n    }\n}'
+    pytest_object = [{"name": "Test 1", "result": "success"}]
 
     grader.grade_pytest_results(pytest_object)
-    with open("validation_results.json") as jsonfile:
-        assert jsonfile.read() == expected_json
+    with open("validation_results.json") as actual_file:
+        with open(
+            "python_pytest_custom_report/config/custom_validation_code/sample_result_2.json"
+        ) as expected_file:
+            assert actual_file.read() == expected_file.read()
 
     # Test clean up
     if os.path.exists("validation_results.json"):
